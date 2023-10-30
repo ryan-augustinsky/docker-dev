64f220f08c9599a38d17c89b4b58b5e34f8f3d9a
Ryan Augustinsky <ryan@augustinsky.ca>
2023-10-30 17:17:09 -0400

    prune dev system after building an image

--- build-base.sh
+++ build-base.sh
@@ -2,4 +2,4 @@ echo "Building $1:latest"
 docker buildx create --use
 docker buildx build . --platform linux/amd64,linux/arm64 --push -t $1:latest
 docker pull $1:latest
-docker image prune --force --filter "REPOSITORY=$1"
+docker image prune --force
