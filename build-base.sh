561ddc86cb71ac8a4e7ffde4b2fb7253099ff3ed
Ryan Augustinsky <ryan@augustinsky.ca>
2023-10-30 22:43:20 -0400

    automated house cleaning to save space

--- build-base.sh
+++ build-base.sh
@@ -1,5 +1,8 @@
 echo "Building $1:latest"
+# fixes a problem with buildx not stopping properly
+docker stop $(docker ps -a -q --filter ancestor=moby/buildkit:buildx-stable-1 --format="{{.ID}}")
+docker system prune --force
+docker volume prune -a -f
 docker buildx create --use
 docker buildx build . --platform linux/amd64,linux/arm64 --push -t $1:latest
 docker pull $1:latest
-docker image prune --force
