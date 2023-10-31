4c0829f98839bedbe657a0e40a4ae67fc5781938
Ryan Augustinsky <ryan@augustinsky.ca>
2023-10-31 18:10:03 +0000

    added a build alias to self-build in devbuntu

--- devbuntu/init.sh
+++ devbuntu/init.sh
@@ -1,3 +1,6 @@
+# symlink all .sh files in /devbuntu/bin to no extension in /devbuntu/bin
+for f in /devbuntu/bin/*.sh; do ln -s $f /devbuntu/bin/$(basename $f .sh); done
+
 cat /run/secrets/docker-access-token | docker login --username=$(cat /run/secrets/docker-username) --password-stdin
 docker plugin install --grant-all-permissions --alias hetzner costela/docker-volume-hetzner
 docker plugin set hetzner apikey=$(cat /run/secrets/hetzner-token)
@@ -15,5 +18,7 @@ EOL
 
 git config --global user.email "$(cat /run/secrets/git-email)"
 git config --global user.name "$(cat /run/secrets/git-name)"
+
+alias build='/code/docker-dev/devbuntu/build.sh'
 
 cd /code
