596983b54d91a63e0190530236591429eb13f941
Ryan Augustinsky <ryan@augustinsky.ca>
2023-10-31 19:05:26 +0000

    script to build every dockerfile in order

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-./ubuntu/build.sh
-./mybuntu/build.sh
-./devbuntu/build.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+BUILD_DIRS=(ubuntu mybuntu devbuntu)
+for BUILD_DIR in ${BUILD_DIRS[@]}; do
+    $SCRIPT_DIR/$BUILD_DIR/build.sh
+done
