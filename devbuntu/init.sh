d35b5d02cecff945fd6ed7140365953e1c672b85
Ryan Augustinsky <ryan@augustinsky.ca>
2023-10-31 21:25:21 +0000

    use vault secrets

--- devbuntu/init.sh
+++ devbuntu/init.sh
@@ -1,24 +1,24 @@
 # symlink all .sh files in /devbuntu/bin to no extension in /devbuntu/bin
 for f in /devbuntu/bin/*.sh; do ln -s $f /devbuntu/bin/$(basename $f .sh); done
 
-cat /run/secrets/docker-access-token | docker login --username=$(cat /run/secrets/docker-username) --password-stdin
+secret docker_access_token | docker login --username=$(secret docker_username) --password-stdin
 docker plugin install --grant-all-permissions --alias hetzner costela/docker-volume-hetzner
-docker plugin set hetzner apikey=$(cat /run/secrets/hetzner-token)
+docker plugin set hetzner apikey=$(secret hetzner_token)
 docker plugin enable hetzner
 
 cat >~/.netrc <<EOL
 machine github.com
-login $(cat /run/secrets/github-username)
-password $(cat /run/secrets/github-token)
+login $(secret github_username)
+password $(secret github_token)
 
 machine api.github.com
-login $(cat /run/secrets/github-username)
-password $(cat /run/secrets/github-token)
+login $(secret github_username)
+password $(secret github_token)
 EOL
 
-git config --global user.email "$(cat /run/secrets/git-email)"
-git config --global user.name "$(cat /run/secrets/git-name)"
+git config --global user.email "$(secret git_email)"
+git config --global user.name "$(secret git_name)"
 
-alias build='/code/docker-dev/devbuntu/build.sh'
+ln -s /code/docker-dev/build.sh /devbuntu/bin/build
 
 cd /code
