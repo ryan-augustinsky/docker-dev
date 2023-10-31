ef6dfcd27544a7f8e0f8d27acffb607bae32555c
Ryan Augustinsky <ryan@augustinsky.ca>
2023-10-31 21:32:47 +0000

    hardcoding docker-dev dir temporariliy

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 BUILD_DIRS=(ubuntu mybuntu devbuntu)
 for BUILD_DIR in ${BUILD_DIRS[@]}; do
-    $SCRIPT_DIR/$BUILD_DIR/build.sh
+    /docker-dev/devbuntu/$BUILD_DIR/build.sh
 done
