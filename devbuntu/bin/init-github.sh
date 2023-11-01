6cf530be8aa9c92825313af4532430db0fb04b32
Ryan Augustinsky <ryan@augustinsky.ca>
2023-11-01 10:13:52 +0000

    init github only non-archived repositories

--- devbuntu/bin/init-github.sh
+++ devbuntu/bin/init-github.sh
@@ -1,7 +1,7 @@
 secret github_token | gh auth login --with-token
 mkdir -p /code/github
 # clone every repo from github to /code/github if it doesn't exist, using gh repo list
-for repo in $(gh repo list --json nameWithOwner --limit 1000 | jq -r '.[].nameWithOwner'); do
+for repo in $(gh repo list --no-archived --json nameWithOwner --limit 1000 | jq -r '.[].nameWithOwner'); do
   if ! [ -d /code/github/$repo ]; then
     gh repo clone $repo /code/github/$repo
   fi
