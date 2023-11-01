edc4234e82758eecbaede538d6511c0ed3a5d3a3
Ryan Augustinsky <ryan@augustinsky.ca>
2023-11-01 10:02:57 +0000

    fixed dir for accessing build-base

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 for d in */ ; do
     IMAGE = $(secret docker_username)/$(basename $d)
     SOURCE_CODE = $SCRIPT_DIR/$(basename $d)
-    $SCRIPT_DIR/../build-base.sh $IMAGE $SOURCE_CODE
+    $SCRIPT_DIR/build-base.sh $IMAGE $SOURCE_CODE
 done
