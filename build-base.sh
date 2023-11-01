131668a7b5702a7057084826f72c9abe9ea31f73
Ryan Augustinsky <ryan@augustinsky.ca>
2023-11-01 07:17:30 +0000

    use remote image caching to speed up docker builds

--- build-base.sh
+++ build-base.sh
@@ -1,8 +1,8 @@
 echo "Building $1:latest"
 # fixes a problem with buildx not stopping properly
 docker stop $(docker ps -a -q --filter ancestor=moby/buildkit:buildx-stable-1 --format="{{.ID}}")
-docker system prune --force
+docker container prune -f
 docker volume prune -a -f
 docker buildx create --use
-docker buildx build $2/. --platform linux/amd64,linux/arm64 --push -t $1:latest
+docker buildx build $2/. --platform linux/amd64,linux/arm64 --push -t $1:latest --cache-to type=registry,ref=$1:cache --cache-from type=registry,ref=$1:cache
 docker pull $1:latest
