e5399dea9d5ec2d54e034cdbf14e34b167a3094d
Ryan Augustinsky <ryan@augustinsky.ca>
2023-11-01 07:24:22 +0000

    fix bash conditional missing then for checking if hetzner plugin is installed

--- devbuntu/init.sh
+++ devbuntu/init.sh
@@ -12,7 +12,7 @@ done
 
 secret docker_access_token | docker login --username=$(secret docker_username) --password-stdin
 
-if ! [ "$(docker plugin inspect hetzner -f '{{.Enabled}}')" ];
+if ! [ "$(docker plugin inspect hetzner -f '{{.Enabled}}')" ]; then
   docker plugin install --grant-all-permissions --alias hetzner costela/docker-volume-hetzner
   docker plugin set hetzner apikey=$(secret hetzner_token)
   docker plugin enable hetzner
