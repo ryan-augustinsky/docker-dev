7ee879d44f45e5a66d5167403c06d3f14043ceba
Ryan Augustinsky <ryan@augustinsky.ca>
2023-11-03 07:38:35 +0000

    prioritize dirs for building dev containers

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-for d in */ ; do
+ORDERED_DIRS=(ubuntu mybuntu devbuntu prodbundu)
+for d in $ORDERED_DIRS; do
     IMAGE=$(secret docker_username)/$(basename $d)
     SOURCE_CODE=$SCRIPT_DIR/$(basename $d)
     $SCRIPT_DIR/build-base.sh $IMAGE $SOURCE_CODE
