19401fba70f0cb2551af40e5a971876634487c69
Ryan Augustinsky <ryan@augustinsky.ca>
2023-11-03 23:00:55 +0000

    renamed devbunto/bin/init to devbuntu

---
+++ devbuntu/bin/devbuntu.sh
@@ -0,0 +1,7 @@
+mkdir -p /code/.vscode-server
+mkdir -p ~/.vscode-server
+ln -s  /code/.vscode-server ~/.vscode-server
+
+init-github
+init-git
+init-containers devbuntu
--- devbuntu/bin/init.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/.vscode-server
-ln -s ~/.vscode-server /code/.vscode-server
-
-init-github
-init-docker
-init-git
-init-containers
