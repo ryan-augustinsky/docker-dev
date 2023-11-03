162301da59ceb68f1749eb8b1f4bff5e15cd0fcb
Ryan Augustinsky <ryan@augustinsky.ca>
2023-11-03 08:12:15 +0000

    disabling building of prodbuntu

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ORDERED_DIRS='ubuntu mybuntu devbuntu prodbundu'
+ORDERED_DIRS='ubuntu mybuntu devbuntu'
 for d in $ORDERED_DIRS; do
     IMAGE=$(secret docker_username)/$(basename $d)
     SOURCE_CODE=$SCRIPT_DIR/$(basename $d)
