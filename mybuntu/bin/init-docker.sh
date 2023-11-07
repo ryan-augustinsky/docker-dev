43dbbda576eefa593216f0a1a9d16d0b46717fe0
Ryan Augustinsky <ryan@augustinsky.ca>
2023-11-07 05:24:06 +0000

    use ubuntu as app for these init secrets

--- mybuntu/bin/init-docker.sh
+++ mybuntu/bin/init-docker.sh
@@ -1,7 +1,7 @@
-secret docker_access_token | docker login --username=$(secret docker_username) --password-stdin
+secret ubuntu docker_access_token | docker login --username=$(secret ubuntu docker_username) --password-stdin
 
 if ! [ "$(docker plugin inspect hetzner -f '{{.Enabled}}')" ]; then
   docker plugin install --grant-all-permissions --alias hetzner costela/docker-volume-hetzner
-  docker plugin set hetzner apikey=$(secret hetzner_token)
+  docker plugin set hetzner apikey=$(secret ubuntu hetzner_token)
   docker plugin enable hetzner
 fi
