34eb64791011a2126014219f5e6d9f0df7f8b3c9
Ryan Augustinsky <ryan@augustinsky.ca>
2023-11-16 06:55:14 +0000

    sync repos on init

--- devbuntu/bin/init-github.sh
+++ devbuntu/bin/init-github.sh
@@ -5,5 +5,8 @@ mkdir -p /mnt/code/github
 for repo in $(gh repo list --no-archived --json nameWithOwner --limit 1000 | jq -r '.[].nameWithOwner'); do
   if ! [ -d /mnt/code/github/$repo ]; then
     gh repo clone $repo /mnt/code/github/$repo
+  else
+    cd /mnt/code/github/$repo
+    gh repo sync
   fi
 done
