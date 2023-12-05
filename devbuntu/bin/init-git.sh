6db822222bc637eccc27f904fa2358ce3891390d
Ryan Augustinsky <ryan@augustinsky.ca>
2023-12-05 20:54:30 +0000

    git config for when mfa is enabled on github

--- devbuntu/bin/init-git.sh
+++ devbuntu/bin/init-git.sh
@@ -8,5 +8,8 @@ login $github_username
 password $github_token
 EOL
 
+# https://stackoverflow.com/a/44247040
+git config --global --add url."git@github.com:".insteadOf "https://github.com/"
+
 git config --global user.email "$git_email"
 git config --global user.name "$git_name"
